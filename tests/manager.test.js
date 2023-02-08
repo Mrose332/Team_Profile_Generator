06d380b37a53bfd865c8332ad6561e12331b7b40
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-02-07 21:52:49 -0500

    made changes to return

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,1 +1,13 @@
 const Manager = require('../lib/Manager');
+
+test("set office number", () => {
+    const officeNumber = "100";
+    const manager = new manager("Barry Allen");
+    expect(employee.officceNumber).toBe(OfficeNumber);
+  });
+  
+  test("getRole() return Intern as a role", () => {
+    const role = "Manager";
+    const employee = new Manager(Barry);
+    expect(employee.getRole()).toBe(role);
+  });
