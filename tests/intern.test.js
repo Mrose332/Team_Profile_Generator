2daec603fb18cb1bdb3effbbb658b24406ebeb58
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-02-07 21:47:26 -0500

    Update intern.test.js

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -1,1 +1,13 @@
 const Intern = require('../lib/Intern');
+
+test("set school using function", () => {
+    const school = "Fsu";
+    const employee = new Intern("Tony Stark");
+    expect(employee.school).toBe(school);
+  });
+  
+  test("getRole() return Intern as a role", () => {
+    const role = "Intern";
+    const employee = new Intern(Tony);
+    expect(employee.getRole()).toBe(role);
+  });
