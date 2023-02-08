57ec60911e8ec7e14fe8ec9aef738a6d5e4e27ca
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-02-07 21:38:00 -0500

    update engineer test to work

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -5,3 +5,10 @@ test("Can make a github user account", () => {
     const e = new Engineer("dan", 1, "dan@gmail.com", testValue);
     expect(e.github).toBe(testValue);
   });
+
+  
+test("getRole() return Engineer as a role", () => {
+  const role = "Engineer";
+  const employee = new Engineer(dan);
+  expect(employee.getRole()).toBe(role);
+});
